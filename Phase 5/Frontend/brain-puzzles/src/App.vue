15e2ba9a81f39f0123431c7ba275521007736550
veljkoselakovic <bg.selakovicveljko@gmail.com>
2022-04-17 18:25:06 +0200

    Demo Vue.js da vidimo da li moze

---
+++ Phase 5/Frontend/brain-puzzles/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+  <div class="title"> BrainPuzzles</div>
+  <CanvasComponent width=50vw height=40vh>
+
+
+
+  </CanvasComponent>
+</template>
+
+<script>
+import CanvasComponent from './components/CanvasComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CanvasComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: #7879F1;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body, html {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  min-height: 100vh;
+  overflow: hidden;
+}
+body {
+  background-color: #7879F1;
+}
+
+.title {
+  position: relative;
+  z-index: 4;
+  top:40%;
+  font-family: 'Pacifico';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 64px;
+  letter-spacing: 0.05em;
+  color:white;
+  margin-bottom: 0;
+  line-height: 1.3em;
+}
+
+</style>
---
+++ Phase 5/Frontend/brain-puzzles/src/components/CanvasComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <div class = "canvas">
+      <img alt="BrainPuzzles logo" class = "logoPicture"  src="../assets/logo.png">
+</div>
+    <div class="blur"></div>
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'CanvasComponent',
+  props: [
+    'width', 'height'
+  ]
+}
+</script>
+
+<style scoped>
+.canvas {
+  position:relative;
+  z-index: 1;
+  margin: 0 auto;
+  width: v-bind(width);
+  height: v-bind(height);
+  background-color: white;
+  border-radius: 2em;
+  box-shadow: 0px 15px 10px 0px rgba(0, 0, 0, 0.25);
+}
+.blur {
+  position:relative;
+  z-index: 0;
+  /* top:7%; */
+  margin: 0 auto;
+  /* width: v-bind(width);
+  height: v-bind(height); */
+  width: calc(v-bind(width) * 1.05);
+  height: calc(v-bind(height) * 1.05);
+  background-color: #F178B6;
+  -webkit-filter: blur(4em);
+  -moz-filter: blur(4em);
+  -o-filter: blur(4em);
+  -ms-filter: blur(4em);
+  filter: blur(4em);
+  
+}
+.container {
+  display:grid;
+  grid-template-columns:1fr;
+}
+.container div { 
+  grid-row-start: 1;
+  grid-column-start:1;
+}
+.logoPicture{
+  width:10vh;
+  height:auto;
+}
+</style>
