75e869c090afc4976871f3a577586c53ebdf716e
ivasljiva <ivaarakic@yahoo.com>
2022-05-21 21:35:29 +0200

    ProfileCanvasComponent, GameCanvasComponent i FancyText

---
+++ Phase 5/Frontend/brain-puzzles/src/components/DashboardComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <header><HeaderComponent/></header>
+    <div id="cont">
+        <div id="left">
+             <FancyText width=20% height=3.5vw text='Hello Iva!' fontSize=25px></FancyText>
+            <br>
+            <ProfileCanvasComponent username="ivasljiva" email="ivaarakic@yahoo.com" status="Gold"
+            aboutMe="Text text text.........." totalScore="2500" highScore="1200" profilePicture=".\profilna2.jpg"></ProfileCanvasComponent>
+        </div>   
+       
+    </div>
+    <footer><FooterComponent/></footer>
+</template>
+
+<script>
+import FooterComponent from './BasicComponents/FooterComponent.vue'
+import HeaderComponent from './BasicComponents/HeaderComponent.vue'
+import ProfileCanvasComponent from './CanvasComponents/ProfileCanvasComponent.vue'
+import FancyText from './BasicComponents/FancyText.vue'
+
+export default {
+    components :{FooterComponent, HeaderComponent, ProfileCanvasComponent, FancyText},
+    name: 'DashboardComponent'
+}
+</script>
+
+<style scoped>
+#cont{
+    margin: 1%;
+}
+</style>
