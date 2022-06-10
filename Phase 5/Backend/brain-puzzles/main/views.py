98f5bb028439edf5ab1d756c8856857bc8c2c664
veljkoselakovic <bg.selakovicveljko@gmail.com>
2022-06-11 01:58:40 +0200

    Vuln fix

--- Phase 5/Backend/brain-puzzles/main/views.py
+++ Phase 5/Backend/brain-puzzles/main/views.py
@@ -395,6 +395,9 @@ class AddThemeView(View):
         # ---------
         # HttpResponse
 
+        if not request.user.is_superuser:
+            return redirect("mainscreen_page")
+            
         
         info = {
             'user' :  request.user.username,
@@ -417,6 +420,10 @@ class AddThemeView(View):
         # Returns
         # ---------
         # HttpResponseRedirect
+
+        if not request.user.is_superuser:
+            return redirect("mainscreen_page")
+            
 
         trenutni_user = request.user
         tema_tekst = request.POST.get('themeName')
@@ -483,6 +490,10 @@ class AddQuestionView(View):
         # Returns
         # ---------
 
+        if not request.user.is_superuser:
+            return redirect("mainscreen_page")
+            
+
         info = {
             'user' :  request.user.username,
             'email' : request.user.email,
@@ -505,6 +516,9 @@ class AddQuestionView(View):
         # ---------
         # HttpResponseRedirect
 
+        if not request.user.is_superuser:
+            return redirect("mainscreen_page")
+            
         trenutni_user = request.user
         pitanje_tekst = request.POST.get('question')
         odgovor_tekst = request.POST.get('answer')
@@ -525,7 +539,7 @@ class AddQuestionView(View):
 
 class AddAdminView(View):
 # Klasa za prikaz stranice za dodavanje admina
-    
+
     @method_decorator(login_required)
     def get(self, request):
         # Dohvatanje get zahteva
@@ -538,6 +552,9 @@ class AddAdminView(View):
         # Returns
         # ---------
         # HttpResponse
+
+        if not request.user.is_superuser:
+            return redirect("mainscreen_page")
 
         info = {
             'user' :  request.user.username,
@@ -561,7 +578,9 @@ class AddAdminView(View):
     #     Returns
     #     ---------
     #     HttpResponseRedirect
-
+        if not request.user.is_superuser:
+            return redirect("mainscreen_page")
+                
         user = request.POST.get('user')
         try:
             newAdmin = Korisnik.objects.filter(username=user)[0]
